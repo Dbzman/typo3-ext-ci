acc69e59be98219c6200807949c6d86e0f03f3cf
Timo Litzius <timo.litzius@aoe.com>
2016-09-23 15:45:11 +0200

    Add composer stuff again

--- run.sh
+++ run.sh
@@ -14,8 +14,13 @@ run() {
 	CWD=`pwd`
 	prepare_files $CWD $1
 
-
-    PHPUNIT=phpunit
+    if (( ${test_travis_flag:-0} == 0 )) ; then
+		curl -sS https://getcomposer.org/installer | php  -d detect_unicode=Off -d apc.enable_cli=Off
+		php -d detect_unicode=Off -d apc.enable_cli=Off composer.phar install --dev
+		PHPUNIT=vendor/bin/phpunit
+	else
+		PHPUNIT=phpunit
+	fi
 
 	cloneorupdate "${CWD}/Core" "git://git.typo3.org/Packages/TYPO3.CMS.git" "${2}"
 	cloneorupdate "${CWD}/t3/typo3conf/ext/phpunit" "git://git.typo3.org/TYPO3CMS/Extensions/phpunit.git" "3.6.10"
